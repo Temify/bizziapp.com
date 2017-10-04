48b7c4180bf1a9719c0f654006abefe118056ef5
Jakub Bartal <kuboo50@gmail.com>
2017-10-04 22:34:23 +0200

    Features

--- public/assets/frontend/js/app.js
+++ public/assets/frontend/js/app.js
@@ -1,1 +1,10 @@
-"use strict";
+'use strict';
+
+window.sr = ScrollReveal();
+sr.reveal('.header__lang');
+sr.reveal('.header__logo');
+sr.reveal('.header__login-wrap');
+sr.reveal('.header__title');
+sr.reveal('.header__description');
+sr.reveal('.header__mail');
+sr.reveal('.header__send');
---
+++ src/frontend/js/global.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal();
+sr.reveal('.header__lang');
+sr.reveal('.header__logo');
+sr.reveal('.header__login-wrap');
+sr.reveal('.header__title');
+sr.reveal('.header__description');
+sr.reveal('.header__mail');
+sr.reveal('.header__send');
