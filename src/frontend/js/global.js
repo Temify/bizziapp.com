7eed31cc4c91bd177626593a3bc5269872d20bc2
Jakub Bartal <kuboo50@gmail.com>
2017-10-07 16:34:58 +0200

    Features

--- src/frontend/js/global.js
+++ src/frontend/js/global.js
@@ -1,8 +1,28 @@
-window.sr = ScrollReveal();
-sr.reveal('.header__lang');
-sr.reveal('.header__logo');
-sr.reveal('.header__login-wrap');
-sr.reveal('.header__title');
-sr.reveal('.header__description');
-sr.reveal('.header__mail');
-sr.reveal('.header__send');
+window.sr = ScrollReveal({
+    duration: 250,
+    delay: 2
+});
+    sr.reveal('*');
+    sr.reveal('.container', {duration: 0});
+
+// On click add class active to language
+
+const lang =  document.getElementsByClassName('header__lang');
+
+lang[1].addEventListener('click', function() {
+
+    if (lang[1].classList.contains('active')) {
+    } else {
+        lang[1].classList.add('active');
+        lang[0].classList.remove('active');
+    }
+});
+
+lang[0].addEventListener('click', function() {
+
+  if (lang[0].classList.contains('active')) {
+  } else {
+      lang[0].classList.add('active');
+      lang[1].classList.remove('active');
+  }
+});
